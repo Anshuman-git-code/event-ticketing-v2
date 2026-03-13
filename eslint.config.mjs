c40d0da38911ad3bb93c65775bbe0083fd668bd9
Anshuman-git-code <anshumanmohapatra.m@gmail.com>
2026-03-13 06:55:50 +0530

    feat: initial CDK project setup with Phase 1 foundation

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'cdk.out/**',
+      '*.js',
+      '*.d.ts',
+      'dist/**',
+      'build/**',
+    ],
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      parser: await import('@typescript-eslint/parser'),
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
